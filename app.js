ad66479b650a7a1a0a8abfbb7eb7dd62340d181d
AJRusch <ajrusch52@gmail.com>
2024-11-22 06:25:27 -0600

    add dotenv

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
 const helmet = require("helmet");
 const mainRouter = require("./routes/index");
 const { searchRecipes, getRecipeSummary } = require("./utils/recipe-api");
