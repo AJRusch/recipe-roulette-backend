1389c6a6eca352668e653514822acd5f04f45ae4
AJRusch <ajrusch52@gmail.com>
2024-11-22 06:13:10 -0600

    modified app

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const cors = require("cors");
 const helmet = require("helmet");
 const mainRouter = require("./routes/index");
 const { searchRecipes, getRecipeSummary } = require("./utils/recipe-api");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
+const { errors } = require("celebrate");
+const errorHandler = require("./middlewares/error-handler");
 
 const app = express();
 const { PORT = 3002 } = process.env;
@@ -22,6 +25,7 @@ mongoose
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use(requestLogger);
 app.use("/", mainRouter);
 app.get("/api/recipes/search", async (req, res) => {
   const searchTerm = req.query.searchTerm;
@@ -37,3 +41,7 @@ app.get("/api/recipes/:recipeId/summary", async (req, res) => {
   const results = await getRecipeSummary(recipeId);
   return res.json(results);
 });
+
+app.use(errorLogger);
+app.use(errors());
+app.use(errorHandler);
